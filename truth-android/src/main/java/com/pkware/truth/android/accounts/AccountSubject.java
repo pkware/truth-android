6d777900bd8a19376f244b103f6731c13f88a9b9
Marius Volkhart <Marius@volkhart.com>
2016-03-09 16:44:30 -0600

    AccountSubject

---
+++ truth-android/src/main/java/com/pkware/truth/android/accounts/AccountSubject.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2016 PKWARE, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.pkware.truth.android.accounts;
+
+import android.accounts.Account;
+
+import com.google.common.truth.FailureStrategy;
+import com.google.common.truth.Subject;
+import com.google.common.truth.SubjectFactory;
+
+import static com.google.common.truth.Truth.assertThat;
+
+/**
+ * Assertions for {@link Account} instances.
+ */
+public class AccountSubject extends Subject<AccountSubject, Account> {
+  private AccountSubject(FailureStrategy failureStrategy, Account subject) {
+    super(failureStrategy, subject);
+  }
+
+  public static SubjectFactory<AccountSubject, Account> type() {
+    return new SubjectFactory<AccountSubject, Account>() {
+      @Override
+      public AccountSubject getSubject(FailureStrategy fs, Account that) {
+        return new AccountSubject(fs, that);
+      }
+    };
+  }
+
+  public AccountSubject hasName(String name) {
+    assertThat(getSubject().name)
+        .named("name")
+        .isEqualTo(name);
+    return this;
+  }
+
+  public AccountSubject hasType(String type) {
+    assertThat(getSubject().type)
+        .named("type")
+        .isEqualTo(type);
+    return this;
+  }
+}

d54e410a909a7d6ad24ea3c1b1a116f5a4a2dc3b
Marius Volkhart <Marius@volkhart.com>
2016-11-24 16:18:03 +0100

    Provide high level Assertions classes as entry points
    This provides an easy way to use the assertions and will be all that most developers need. This does produce a limitation though. By generating the code, we need all of the `SubjectFactory`s to be exposed in a predictable way, so providing custom anf human-friendly static factory methods on each Subject is not possible.

---
+++ truth-android-mediarouter-v7/src/main/java/com/pkware/truth/android/mediarouter/v7/Assertions.java
@@ -0,0 +1,64 @@
+// Copyright 2016 PKWARE, Inc.
+//
+// This class is generated. Do not modify directly!
+package com.pkware.truth.android.mediarouter.v7;
+
+import com.google.common.truth.SubjectFactory;
+
+import static com.google.common.truth.Truth.assertAbout;
+
+/** Assertions for testing Android classes. */
+@SuppressWarnings("deprecation")
+public final class Assertions {
+  public static com.pkware.truth.android.mediarouter.v7.media.MediaItemStatusSubject assertThat(
+      android.support.v7.media.MediaItemStatus target) {
+    SubjectFactory<com.pkware.truth.android.mediarouter.v7.media.MediaItemStatusSubject, android.support.v7.media.MediaItemStatus> type = com.pkware.truth.android.mediarouter.v7.media.MediaItemStatusSubject.type();
+    return assertAbout(type).that(target);
+  }
+
+  public static com.pkware.truth.android.mediarouter.v7.media.MediaRouteDescriptorSubject assertThat(
+      android.support.v7.media.MediaRouteDescriptor target) {
+    SubjectFactory<com.pkware.truth.android.mediarouter.v7.media.MediaRouteDescriptorSubject, android.support.v7.media.MediaRouteDescriptor> type = com.pkware.truth.android.mediarouter.v7.media.MediaRouteDescriptorSubject.type();
+    return assertAbout(type).that(target);
+  }
+
+  public static com.pkware.truth.android.mediarouter.v7.media.MediaRouteDiscoveryRequestSubject assertThat(
+      android.support.v7.media.MediaRouteDiscoveryRequest target) {
+    SubjectFactory<com.pkware.truth.android.mediarouter.v7.media.MediaRouteDiscoveryRequestSubject, android.support.v7.media.MediaRouteDiscoveryRequest> type = com.pkware.truth.android.mediarouter.v7.media.MediaRouteDiscoveryRequestSubject.type();
+    return assertAbout(type).that(target);
+  }
+
+  public static com.pkware.truth.android.mediarouter.v7.media.MediaRouteProviderProviderMetadataSubject assertThat(
+      android.support.v7.media.MediaRouteProvider.ProviderMetadata target) {
+    SubjectFactory<com.pkware.truth.android.mediarouter.v7.media.MediaRouteProviderProviderMetadataSubject, android.support.v7.media.MediaRouteProvider.ProviderMetadata> type = com.pkware.truth.android.mediarouter.v7.media.MediaRouteProviderProviderMetadataSubject.type();
+    return assertAbout(type).that(target);
+  }
+
+  public static com.pkware.truth.android.mediarouter.v7.media.MediaRouterProviderInfoSubject assertThat(
+      android.support.v7.media.MediaRouter.ProviderInfo target) {
+    SubjectFactory<com.pkware.truth.android.mediarouter.v7.media.MediaRouterProviderInfoSubject, android.support.v7.media.MediaRouter.ProviderInfo> type = com.pkware.truth.android.mediarouter.v7.media.MediaRouterProviderInfoSubject.type();
+    return assertAbout(type).that(target);
+  }
+
+  public static com.pkware.truth.android.mediarouter.v7.media.MediaRouterRouteInfoSubject assertThat(
+      android.support.v7.media.MediaRouter.RouteInfo target) {
+    SubjectFactory<com.pkware.truth.android.mediarouter.v7.media.MediaRouterRouteInfoSubject, android.support.v7.media.MediaRouter.RouteInfo> type = com.pkware.truth.android.mediarouter.v7.media.MediaRouterRouteInfoSubject.type();
+    return assertAbout(type).that(target);
+  }
+
+  public static com.pkware.truth.android.mediarouter.v7.media.MediaSessionStatusSubject assertThat(
+      android.support.v7.media.MediaSessionStatus target) {
+    SubjectFactory<com.pkware.truth.android.mediarouter.v7.media.MediaSessionStatusSubject, android.support.v7.media.MediaSessionStatus> type = com.pkware.truth.android.mediarouter.v7.media.MediaSessionStatusSubject.type();
+    return assertAbout(type).that(target);
+  }
+
+  public static com.pkware.truth.android.mediarouter.v7.media.RemotePlaybackClientSubject assertThat(
+      android.support.v7.media.RemotePlaybackClient target) {
+    SubjectFactory<com.pkware.truth.android.mediarouter.v7.media.RemotePlaybackClientSubject, android.support.v7.media.RemotePlaybackClient> type = com.pkware.truth.android.mediarouter.v7.media.RemotePlaybackClientSubject.type();
+    return assertAbout(type).that(target);
+  }
+
+  private Assertions() {
+    throw new AssertionError("No instances.");
+  }
+}
